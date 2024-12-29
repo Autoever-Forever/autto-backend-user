cf150d2c8b225dae0921c3e23ebdf4bcbc4dae5d
imdanaa <jangs1920@gmail.com>
2024-12-29 17:39:45 +0900

    AF-73 feat: 로그인 기능 - jwt 토큰 관련 설정 추가

---
+++ UserService/src/main/java/com/autto/userservice/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.autto.userservice.config;
+
+import com.autto.userservice.jwt.JwtAuthenticationFilter;
+import com.autto.userservice.provider.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                // REST API 이므로 basic auth 및 csrf 보안을 사용하지 않음
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                // JWT 를 사용하기 때문에 세션을 사용하지 않음
+                .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        // 해당 API에 대해서는 모든 요청을 허가
+                        .requestMatchers("/auth/sign-up", "/auth/sign-in").permitAll()
+                        // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
+                        .anyRequest().authenticated())
+                        // USER 권한이 있어야 요청할 수 있음
+//                        .requestMatchers("/auth/test").hasRole("USER"))
+                // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 비밀번호 인코딩
+        // BCrypt Encoder 사용
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
