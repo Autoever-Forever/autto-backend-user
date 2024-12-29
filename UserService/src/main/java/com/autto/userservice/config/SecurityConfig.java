5cd5b7851d7222d2dd10a3fae85a4651ccdc26b3
imdanaa <jangs1920@gmail.com>
2024-12-29 17:55:39 +0900

    AF-73 fix: Spring Security 설정 코드 수정

--- UserService/src/main/java/com/autto/userservice/config/SecurityConfig.java
+++ UserService/src/main/java/com/autto/userservice/config/SecurityConfig.java
@@ -21,6 +21,13 @@ public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 비밀번호 인코딩
+        // BCrypt Encoder 사용
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 // REST API 이므로 basic auth 및 csrf 보안을 사용하지 않음
@@ -31,22 +38,16 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         // 해당 API에 대해서는 모든 요청을 허가
-                        .requestMatchers("/auth/sign-up", "/auth/sign-in").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()
                         // USER 권한이 있어야 요청할 수 있음
-//                        .requestMatchers("/auth/test").hasRole("USER"))
+//                        .requestMatchers("/auth/test").hasRole("USER")
+                )
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
 
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // 비밀번호 인코딩
-        // BCrypt Encoder 사용
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
